7c60d757974fcec67fac1f376ccda6820c6b1c84
youth2ee <54021593+youth2ee@users.noreply.github.com>
2019-09-25 17:43:42 +0900

    190925PM5

---
+++ src/com/naver/n1/Client.java
@@ -0,0 +1,94 @@
+package com.naver.n1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client { // 클라이언트는 하나의 서버에게 보낸다. 
+
+	public static void main(String[] args) {
+		//1단계
+		Socket sc=null; //소켓을 만들면 1번 일어남
+
+		OutputStream os=null;
+		OutputStreamWriter ow=null;
+		BufferedWriter bw=null;
+
+		InputStream is=null;
+		InputStreamReader ir=null;
+		BufferedReader br=null;
+
+		Scanner scc = new Scanner(System.in);
+
+		try {
+			sc = new Socket("211.238.142.30", 8282);
+			//String host : ip주소나 도메인주소를 적는다. 
+			//int port : 포트번호를 적는다.
+
+			boolean check =true;
+
+			while(check) {
+				//보내기
+				os = sc.getOutputStream(); //내보내기
+				ow = new OutputStreamWriter(os); // char를 내보낸다.
+				bw = new BufferedWriter(ow); // 문자열String을 내보낸다.
+
+				System.out.println("서버로 전송할 메시지를 입력하세요");
+				String str = scc.next();
+
+				if(str.toUpperCase().equals("Q")) {
+					System.out.println("종료합니다.");
+					break;
+				}
+
+				bw.write(str+"\r\n");
+				bw.flush(); //버퍼를 강제로 내보내기
+				System.out.println("서버로 전송완료");
+
+
+				//받기
+				//연결되어 있으므로 다시 연결할 필요가 없다.
+				System.out.println("클라이언트 접속 받기를 기다리는 중");
+
+				is = sc.getInputStream(); 
+				ir = new InputStreamReader(is);
+				br = new BufferedReader(ir); 
+
+				str = br.readLine();
+				if(str==null) {
+					System.out.println("종료합니다.");
+					break;
+				}
+				System.out.println(str);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+				ir.close();
+				is.close();
+				bw.close();
+				ow.close();
+				os.close();
+				scc.close();
+				sc.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+}
---
+++ src/com/naver/n1/Server.java
@@ -0,0 +1,101 @@
+package com.naver.n1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server { //서버는 다수에게 보낸다.
+
+	public static void main(String[] args) { 
+		//서버는 다수에게 보내기 때문에 소켓을 만들지 않는다. 소켓 만들면 1번만 만들어지므로
+		//서버는 서버소켓이 필요하다
+		//ServerSocket
+		ServerSocket ss =null;
+		Socket sc = null;
+
+		InputStream is=null;
+		InputStreamReader ir=null;
+		BufferedReader br=null;
+
+		OutputStream os=null;
+		OutputStreamWriter ow=null;
+		BufferedWriter bw=null;
+
+		Scanner scc = new Scanner(System.in);
+
+		try {
+			ss = new ServerSocket(8282);
+			//누군가가 서버에 접속하기를 기다리는 애, 포트번호로 들어오는 신호를 기다리는 애
+			//클라이언트가 포트번호 8282를 보냄
+
+			System.out.println("클라이언트 접속 받기를 기다리는 중");
+			sc = ss.accept();
+			//클라이언트가 보낸 걸로  소켓을 만들어준다.
+
+			boolean check = true;
+
+			while(check) {
+				//받기
+				is = sc.getInputStream(); //byte 처리
+				ir = new InputStreamReader(is); //char 처리
+				br = new BufferedReader(ir); //string으로 처리
+
+				String str = br.readLine(); //읽어들이기 
+				if(str==null) {
+					System.out.println("종료합니다.");
+					break;
+				}
+
+				System.out.println(str);
+
+
+				//보내기
+				//연결되어 있으므로 다시 연결할 필요가 없다.
+				os = sc.getOutputStream(); 
+				ow = new OutputStreamWriter(os); 
+				bw = new BufferedWriter(ow);
+
+				System.out.println("클라이언트에게 전송할 메시지를 입력하세요");
+				str = scc.next();
+
+				if(str.toUpperCase().equals("Q")) {
+					System.out.println("종료합니다.");
+					break;
+				}
+
+				bw.write(str+"\r\n");
+				bw.flush(); 
+				System.out.println("클라이언트에게 전송완료");
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				bw.close();
+				ow.close();
+				os.close();
+				scc.close();
+				br.close();
+				ir.close();
+				is.close();
+				sc.close();
+				ss.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+
+	}
+
+}
